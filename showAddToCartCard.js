d53f744815e7638868537b75432a76834a609be1
Yash gunjal <yashgunjal8888@gmail.com>
2025-04-04 07:37:48 +0530

    Resolved merge conflicts in package.json and package-lock.jsonqq

--- showAddToCartCard.js
+++ showAddToCartCard.js
@@ -1,4 +1,5 @@
 import products from "./api/products.json";
+import { fetchQuantityFromCartLS } from "./fetchQuantityFromCartLS";
 import { getCartProductFromLS } from "./getCartProducts";
 
 let cartProducts = getCartProductFromLS();
@@ -8,3 +9,42 @@ let filterProducts = products.filter((curProd) => {
 });
 
 console.log(filterProducts);
+// to update the cart page
+
+const cartElement = document.querySelector("#productCartContainer");
+const templateContainer = document.querySelector("#productCartTemplate");
+
+const showCartProduct = () => {
+  filterProducts.forEach((curProd) => {
+    const { category, id, image, name, stock, price } = curProd;
+
+    let productClone = document.importNode(templateContainer.content, true);
+
+    const lSActualData = fetchQuantityFromCartLS(id, price);
+
+    productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
+    productClone.querySelector(".category").textContent = category;
+    productClone.querySelector(".productName").textContent = name;
+    productClone.querySelector(".productImage").src = image;
+
+    productClone.querySelector(".productQuantity").textContent =
+      lSActualData.quantity;
+    productClone.querySelector(".productPrice").textContent =
+      lSActualData.price;
+
+    // handle increment and decrement button
+    productClone
+      .querySelector(".stockElement")
+      .addEventListener("click", (event) => {
+        incrementDecrement(event, id, stock, price);
+      });
+
+    productClone
+      .querySelector(".remove-to-cart-button")
+      .addEventListener("click", () => removeProdFromCart(id));
+
+    cartElement.appendChild(productClone);
+  });
+};
+
+showCartProduct();
